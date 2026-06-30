738dd61378c6ca278a45e9629cf48fa39d28af0a
LeeJiHyeon <maryanna0427@hanmail.net>
2026-07-01 00:15:51 +0900

    chore: 프로젝트 초기 설정

---
+++ swapclass/src/main/java/com/sookmyung/swapclass/SwapclassApplication.java
@@ -0,0 +1,13 @@
+package com.sookmyung.swapclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapclassApplication.class, args);
+	}
+
+}
