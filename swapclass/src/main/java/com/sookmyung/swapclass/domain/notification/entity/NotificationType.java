08971eeac89de887db5b4f6a1c4ba37f21a62f64
LeeJiHyeon <maryanna0427@hanmail.net>
2026-07-03 02:04:07 +0900

    Notification entity

---
+++ swapclass/src/main/java/com/sookmyung/swapclass/domain/notification/entity/NotificationType.java
@@ -0,0 +1,13 @@
+package com.sookmyung.swapclass.domain.notification.entity;
+
+public enum NotificationType {
+    MATCH_PROPOSAL,
+    RESULT,
+    LIKE,
+    VERIFY_SOON,
+    VERIFY_START,
+    SWAP_RESULT,
+    CANCEL,
+    PENALTY,
+    SYSTEM
+}
