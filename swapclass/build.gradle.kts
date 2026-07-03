fa0efca424ba8810f1c740ee92e8b92f677575f3
vortexjml <vortexjml@sookmyung.ac.kr>
2026-07-03 12:37:51 +0900

    chore: swagger 설정

--- swapclass/build.gradle.kts
+++ swapclass/build.gradle.kts
@@ -33,6 +33,9 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
+	// Swagger (springdoc-openapi, Spring Boot 3.x) - API 문서 자동 생성
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.17")
+
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.projectlombok:lombok")
