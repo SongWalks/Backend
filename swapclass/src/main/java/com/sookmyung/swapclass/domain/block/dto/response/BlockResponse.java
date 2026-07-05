705819e8424133b73eec9ecdff56e74f90c3b236
LeeJiHyeon <maryanna0427@hanmail.net>
2026-07-05 09:09:29 +0900

    feat: 차단(Block) 도메인 구현 (#8)

---
+++ swapclass/src/main/java/com/sookmyung/swapclass/domain/block/dto/response/BlockResponse.java
@@ -0,0 +1,10 @@
+package com.sookmyung.swapclass.domain.block.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BlockResponse {
+    private boolean blocked;
+}
